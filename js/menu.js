9ac3e89fa1d162fc69f02e131ec9a1f2e681c3ae
Dmitry Bartaevich <2005Dimas0112@gmail.com>
2025-12-08 19:54:42 +0300

    beginning modal

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,51 @@
-const btnCoffee = document.querySelector(".coffee");
-const btnTea = document.querySelector(".tea");
-const btnDessert = document.querySelector(".dessert");
-const cardsCoffee = document.querySelectorAll(".card__coffee");
-const cardsTeas = document.querySelectorAll(".card__tea");
-const cardsDesserts = document.querySelectorAll(".card__dessert");
-
 window.addEventListener("DOMContentLoaded", () => {
 
+    const modalOverlay = document.querySelector(".modal__overlay");
+    const modal = document.querySelector(".modal__coffee__one");
+    const btnCoffee = document.querySelector(".coffee");
+    const btnTea = document.querySelector(".tea");
+    const btnDessert = document.querySelector(".dessert");
+    const cardsCoffee = document.querySelectorAll(".card__coffee");
+    const cardsTeas = document.querySelectorAll(".card__tea");
+    const cardsDesserts = document.querySelectorAll(".card__dessert");
+    const btnsSize = document.querySelectorAll('.btns__size');
+    const modalBtnActive = document.querySelector('.modal--btn--active');
+    const btnSize = document.querySelector('.btn__size');
+
+    function closeModal(e) {
+        e.style.display = "none"
+    };
+
+    function openModal(e) {
+        e.style.display = "block"
+    }
+
+    function modalStartBtn() {
+        if (modal.style.display = 'none') {
+            sizeOne.classList.add('modal--btn--active');
+            sizeTwo.classList.remove('modal--btn--active');
+            sizeThree.classList.remove('modal--btn--active');
+        }
+        
+        if (modal.style.display = 'none') {
+            additiveOne.classList.remove('modal--btn--active');
+            additiveTwo.classList.remove('modal--btn--active');
+            additiveThree.classList.remove('modal--btn--active');
+        }
+
+    }
+
     btnCoffee.addEventListener('click', () => {
-        cardsCoffee.forEach((i) => {
-            i.style.display = "block"
+        cardsCoffee.forEach((e) => {
+            openModal(e);
         })
     
-        cardsTeas.forEach((i) => {
-            i.style.display = "none"
+        cardsTeas.forEach((e) => {
+            closeModal(e);
         })
     
-        cardsDesserts.forEach((i) => {
-            i.style.display = "none"
+        cardsDesserts.forEach((e) => {
+            closeModal(e);
         })
     
         btnCoffee.classList.add("btn--active");
@@ -27,16 +55,16 @@ window.addEventListener("DOMContentLoaded", () => {
     })
     
     btnTea.addEventListener('click', () => {
-        cardsCoffee.forEach((i) => {
-            i.style.display = "none"
+        cardsCoffee.forEach((e) => {
+            e.style.display = "none"
         })
     
-        cardsTeas.forEach((i) => {
-            i.style.display = "block"
+        cardsTeas.forEach((e) => {
+            e.style.display = "block"
         })
     
-        cardsDesserts.forEach((i) => {
-            i.style.display = "none"
+        cardsDesserts.forEach((e) => {
+            e.style.display = "none"
         })
     
         btnCoffee.classList.remove("btn--active");
@@ -47,22 +75,102 @@ window.addEventListener("DOMContentLoaded", () => {
     })
     
     btnDessert.addEventListener('click', () => {
-        cardsCoffee.forEach((i) => {
-            i.style.display = "none"
+        cardsCoffee.forEach((e) => {
+            closeModal(e);
         })
     
-        cardsTeas.forEach((i) => {
-            i.style.display = "none"
+        cardsTeas.forEach((e) => {
+            closeModal(e);
         })
     
-        cardsDesserts.forEach((i) => {
-            i.style.display = "block"
+        cardsDesserts.forEach((e) => {
+            openModal(e);
         })
-    
+
         btnCoffee.classList.remove("btn--active");
         btnTea.classList.remove("btn--active");
         btnDessert.classList.add("btn--active");
     
     })
+    
+
+    cardsCoffee.forEach((e) => {
+        e.addEventListener('click', () => {
+            modalOverlay.style.display = "block";
+            modal.style.display = "flex";
+        })
+    })
+
+    document.querySelector('.modal__btn').addEventListener('click', () => {
+        modalOverlay.style.display = "none";
+        modal.style.display = "none";
+        modalStartBtn();
+    })
+
+
+    modalOverlay.addEventListener('click', () => {
+        modalOverlay.style.display = 'none';
+        modal.style.display = 'none';
+        modalStartBtn();
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            modalOverlay.style.display = "none";
+            modal.style.display = "none";
+            modalStartBtn();
+        }
+    })
+
+        
+    const sizeOne = document.querySelector('.size__one');
+    const sizeTwo = document.querySelector('.size__two');
+    const sizeThree = document.querySelector('.size__three');
+
+    const additiveOne = document.querySelector('.additive__one');
+    const additiveTwo = document.querySelector('.additive__two');
+    const additiveThree = document.querySelector('.additive__three');
+
+    sizeOne.addEventListener('click', () => {
+        sizeOne.classList.add('modal--btn--active');
+        sizeTwo.classList.remove('modal--btn--active');
+        sizeThree.classList.remove('modal--btn--active');
+    })
+
+    sizeTwo.addEventListener('click', () => {
+        sizeOne.classList.remove('modal--btn--active');
+        sizeTwo.classList.add('modal--btn--active');
+        sizeThree.classList.remove('modal--btn--active');
+        
+    })
+
+    sizeThree.addEventListener('click', () => {
+        sizeOne.classList.remove('modal--btn--active');
+        sizeTwo.classList.remove('modal--btn--active');
+        sizeThree.classList.add('modal--btn--active');
+    })
+    
+    
+    additiveOne.addEventListener('click', () => {
+        additiveOne.classList.add('modal--btn--active');
+        additiveTwo.classList.remove('modal--btn--active');
+        additiveThree.classList.remove('modal--btn--active');
+    })
+
+    additiveTwo.addEventListener('click', () => {
+        additiveOne.classList.remove('modal--btn--active');
+        additiveTwo.classList.add('modal--btn--active');
+        additiveThree.classList.remove('modal--btn--active');
+    })
+
+    additiveThree.addEventListener('click', () => {
+        additiveOne.classList.remove('modal--btn--active');
+        additiveTwo.classList.remove('modal--btn--active');
+        additiveThree.classList.add('modal--btn--active');
+    })
+    
+    
+    
+
     
 })
