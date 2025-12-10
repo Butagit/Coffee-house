562249da9af98b0983462e7a91d09537f3584b45
Dmitry Bartaevich <2005Dimas0112@gmail.com>
2025-12-10 22:02:36 +0300

    burger menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 const burgerBtn = document.querySelector('.coffee__menu__phone');
 const burgerMenu = document.querySelector('.burger__menu');
+const nav = document.querySelectorAll('.navigation');
 
 burgerBtn.addEventListener('click', () => {
     burgerMenu.style.display = 'flex';
     burgerMenu.classList.toggle('burger--active');
+})
+
+nav.forEach(btn => {
+    btn.addEventListener('click', () => {
+        burgerMenu.style.display = 'none';
+        burgerMenu.classList.toggle('burger--active');
+    })
 })
 
 
--- js/menu.js
+++ js/menu.js
@@ -169,7 +169,21 @@ window.addEventListener("DOMContentLoaded", () => {
         additiveThree.classList.add('modal--btn--active');
     })
     
-    
+    const burgerBtn = document.querySelector('.coffee__menu__phone');
+    const burgerMenu = document.querySelector('.burger__menu');
+    const nav = document.querySelectorAll('.navigation');
+
+    burgerBtn.addEventListener('click', () => {
+        burgerMenu.style.display = 'flex';
+        burgerMenu.classList.toggle('burger--active');
+    })
+
+    nav.forEach(btn => {
+        btn.addEventListener('click', () => {
+            burgerMenu.style.display = 'none';
+            burgerMenu.classList.toggle('burger--active');
+        })
+    })
     
 
     
