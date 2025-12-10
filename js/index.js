0d626ff9c61ac32f64530425afd5ee61052b0930
Dmitry Bartaevich <2005Dimas0112@gmail.com>
2025-12-10 21:33:51 +0300

    test burger

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const burgerBtn = document.querySelector('.coffee__menu__phone');
 const burgerMenu = document.querySelector('.burger__menu');
 
 burgerBtn.addEventListener('click', () => {
+    burgerMenu.style.display = 'flex';
     burgerMenu.classList.toggle('burger--active');
 })
 
